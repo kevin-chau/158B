890712f35cb56f348e3b73490ec3616b1c8baa76
Kevin Chau <kevinchau321@gmail.com>
2016-03-10 10:40:22 -0800

    cleaned up patch, fixed display with sam's solution, working on displaying root notes

--- max enabled/wacom/wacomdisplay.c
+++ max enabled/wacom/wacomdisplay.c
@@ -1,5 +1,5 @@
-/display/width = /size[[1]],
-/display/height = /size[[2]],
+/display/width = 500,
+/display/height = 500,
 
 /partial/x = /display/width / 8,
 /partial/y = /display/height / 8,
@@ -14,7 +14,14 @@ map(
 		assign("/start/y", /partial/y * j),
 		assign("/end/x", /partial/x * (i+1)),
 		assign("/end/y", /partial/y * (j+1)),
-		assign("/draw/"+i+j , ["framerect",/start/x,/start/y,/end/x,/end/y,0,0,0])
+
+		assign("/gridnum/disp", i + 8*(7-j)),
+		assign("/isroot" , ((value("/gridnum/disp")%8) == 0)),
+
+		if(/isroot,
+			assign("/draw/"+i+j , ["framerect",/start/x,/start/y,/end/x,/end/y,200,0,0]),
+			assign("/draw/"+i+j , ["framerect",/start/x,/start/y,/end/x,/end/y,0,0,0])
+		)
   ),
 aseq(0, 7))),
 aseq(0, 7)
@@ -28,10 +35,6 @@ map (
 			assign("/grid/x/"+z , value("/grid/i/"+z)),
 			assign("/grid/y/"+z , div(value("/gridnum/"+z) - value("/grid/x/"+z),8)),
 			assign("/grid/j/"+z , 7 - value("/grid/y/"+z)),
-			assign("/prev/grid/i/"+z , value("/previous/gridnum/"+z) % 8),
-			assign("/prev/grid/x/"+z , value("/prev/grid/i/"+z)),
-			assign("/prev/grid/y/"+z , div(value("/previous/gridnum"+z) - value("/prev/grid/x/"+z),8)),
-			assign("/prev/grid/j/"+z , 7 - value("/prev/grid/y/"+z)),
 
 
 			if(value("/finger/down/"+z), 
@@ -50,18 +53,9 @@ map (
 					assign("/draw/fingerpaint"+z , ["paintrect",value("/start/x/"+z),value("/start/y/"+z),value("/end/x/"+z),value("/end/y/"+z),256,256,256]),
 					assign("/draw/fingerframe"+z , ["framerect",value("/start/x/"+z),value("/start/y/"+z),value("/end/x/"+z),value("/end/y/"+z),0,0,0])
 				)
-			),
-
-			if(value("/previous/gridnum/"+z) != value("/gridnum/"+z),
-				progn(
-					assign("/start/x/prev/"+z, /partial/x * value("/prev/grid/i/"+z)),
-					assign("/start/y/prev/"+z, /partial/y * value("/prev/grid/j/"+z)),
-					assign("/end/x/prev/"+z, /partial/x * (value("/prev/grid/i/"+z)+1)),
-					assign("/end/y/prev/"+z, /partial/y * (value("/prev/grid/j/"+z)+1)),
-					assign("/draw/fingerpaintprev"+z , ["paintrect",value("/start/x/prev/"+z),value("/start/y/prev/"+z),value("/end/x/prev/"+z),value("/end/y/prev/"+z),256,256,256]),
-					assign("/draw/fingerframeprev"+z , ["framerect",value("/start/x/prev/"+z),value("/start/y/prev/"+z),value("/end/x/prev/"+z),value("/end/y/prev/"+z),0,0,0])
-				),
-				0)
+			)
 	),
 	aseq(0,9)
-)
+),
+
+/clear = "clear"
